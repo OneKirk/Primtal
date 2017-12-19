b88ba68e5794e76a0b4b7f128a9d7275ff69d10f
Thomas Kirk <tukirk@gmail.com>
2017-12-19 07:23:39 +0100

    This is the available version in App Store

--- Primtal/ViewController.swift
+++ Primtal/ViewController.swift
@@ -10,6 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var inputField: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func isItPrime(_ sender: Any) {
+        
+        var isPrime = true
+        
+        if let userInput = Int(inputField.text!) {
+            
+            if userInput > 0 {
+                
+                var i = 2
+                
+                while i < userInput && isPrime == true {
+                    
+                    if userInput % i == 0 {
+                        isPrime = false
+                    }
+                
+                    i += 1
+                }
+                
+                if userInput == 1 {
+                    
+                    resultLabel.text = String(userInput) + NSLocalizedString("Not prime", comment: "Message")
+                } else if isPrime {
+                    
+                    resultLabel.text = String(userInput) + NSLocalizedString("Is prime", comment: "Message")
+                } else {
+                    
+                    resultLabel.text = String(userInput) + NSLocalizedString("Not prime", comment: "Message")
+                }
+            } else {
+                
+                resultLabel.text = NSLocalizedString("Invalid input", comment: "Message")
+            }
+        } else {
+            
+            resultLabel.text = NSLocalizedString("Invalid input", comment: "Message")
+            
+        }
+        
+        self.view.endEditing(true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +64,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
 }
 
